b848f9edb820b17bb68886b9987c9ff957b0a2dd
EarthIsHeaven <thinleylama44@gmail.com>
2023-10-21 17:17:27 +0530

    Done

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  // create one worker per available core
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i
+    });
+  }
+  
+  // set up the adapter on the primary thread
+  setupPrimary();
+} else {
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+    // set up the adapter on each worker thread
+    adapter: createAdapter()
+  });
+
+  // [...]
+
+  // each worker will listen on a distinct port
+  const port = process.env.PORT;
+
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
 io.on('connection', async (socket) => {
-  socket.on('chat message', async (msg) => {
+  socket.on('chat message', async (msg, clientOffset, callback) => {
     let result;
     try{
-      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
     } catch (e) {
+      if(e.errno === 19){
+        callback();
+      } else {
+
+      }
       return;
     }
     io.emit('chat message', msg, result.lastID);
+    callback();
   });
   if (!socket.recovered) {
     // if the connection state recovery was not successful
@@ -43,3 +78,4 @@ io.on('connection', async (socket) => {
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
 });
+}
